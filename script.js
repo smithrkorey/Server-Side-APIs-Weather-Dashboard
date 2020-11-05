7119afa4cf2929b78c3dad2a9ead325c72c5e31f
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2020-11-04 20:11:36 -0700

    js edits

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-//insert search-city on click into document ready function
-
 $( document ).ready(function() {
-    
-    //localStorage.setItem('key', 'value')
-    
-    /*for (var index = 0; index < 11; index++) {
-    $("#h" + index).val(localStorage.getItem("hInput"));
-    }*/
 
-    //Example
-    /*var searchHistory = []
-    localStorage.setItem("history", json.stringify(searchHistory));
-    searchHistory = json.parse(localStorage.getItem("history"));*/
+    $("#previousSearch").text((localStorage.getItem("hInput")));
+    console.log(localStorage.getItem("hInput"))
     
-    $("#search-city").on("click", function() {
-        var searchCity = $("#input-search").val();
-        console.log(searchCity);
+    //on click function for previous search city listed on page
+    $("#previousSearch").on("click", function() {
+    
+        var searchCity = $("#previousSearch").text();
+
         $.ajax({
             url: "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather",
             data: {
@@ -25,7 +17,60 @@ $( document ).ready(function() {
             },
             success: function( result ) {
                 //console.log(result)
-                //Add in humidity, wind speed and UV Index
+                
+                $("#city-name").text(searchCity);
+                $("#input-main-temp").text(result.main.temp);
+                $("#input-main-humidity").text(result.main.humidity);
+                $("#input-main-wind-speed").text(result.wind.speed);
+                $.ajax({
+                    url: "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall",
+                    data: {
+                        lat: result.coord.lat,
+                        lon: result.coord.lon,
+                        appid: "50d4c88b6039a0b2d81971032a239e46"
+                    },
+                    success: function( result ) {
+                        $("#input-main-uv-index").text(result.current.uvi);
+                        //console.log(result)
+                        
+                        //Creating for loop to show 5 day forecast results
+                        for (let index = 1; index < 6; index++) {
+                            var forecast = result.daily[index];
+                            
+                            // Convert timestamp to milliseconds
+                            var date = new Date(forecast.dt*1000);
+                            $("#day" + index).text(date.toLocaleDateString("en-US"));
+                            $("#input-temp-day" + index).text(forecast.temp.day);
+                            $("#input-humidity-day" + index).text(forecast.humidity);
+                        }
+                    },
+                });
+            }
+        });
+
+
+
+    });
+
+
+    //on click function for search input button
+    $("#search-city").on("click", function() {
+        var searchCity = $("#input-search").val();
+
+        localStorage.setItem("hInput", searchCity);
+        console.log(searchCity);
+        
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather",
+            data: {
+                q: searchCity,
+                appid: "50d4c88b6039a0b2d81971032a239e46",
+                units: "imperial"
+            },
+            success: function( result ) {
+                //console.log(result)
+                
+                $("#city-name").text(searchCity);
                 $("#input-main-temp").text(result.main.temp)
                 $("#input-main-humidity").text(result.main.humidity)
                 $("#input-main-wind-speed").text(result.wind.speed)
@@ -40,7 +85,7 @@ $( document ).ready(function() {
                         $("#input-main-uv-index").text(result.current.uvi);
                         //console.log(result)
                         
-                        //Creating for loop to show 
+                        //Creating for loop to show 5 day forecast results
                         for (let index = 1; index < 6; index++) {
                             var forecast = result.daily[index];
                             
@@ -55,13 +100,8 @@ $( document ).ready(function() {
             }
         }); 
 
-
-        /*for (var index = 0; index < 11; index++) {
             
-            searchCity = $("#h" + index).val();
     
-            localStorage.setItem("#h" + index, "hInput");
-        }*/
 
     }); 
 
